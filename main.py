b35352f32a0a2013609b376901cbf163be6a8e26
Dmitrii <deigumnov@gmail.com>
2020-12-16 17:23:02 +1000

    Added default value

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# url = 'http://py4e-data.dr-chuck.net/comments_42.html'
-url = input('Enter URL: ')
-count = int(input('Enter count: '))
-pos = int(input('Enter position: '))
+# url = input('Enter URL: ')
+# count = int(input('Enter count: '))
+# pos = int(input('Enter position: '))
+url = 'http://py4e-data.dr-chuck.net/known_by_Albert.html'
+count = 7
+pos = 18
 
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
