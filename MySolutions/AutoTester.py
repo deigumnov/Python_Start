108adf4a9dcf182f1e64a981215bf2762be34fde
Dmitrii <deigumnov@gmail.com>
2021-02-17 21:59:42 +1000

    Looks like better name for const variables

--- MySolutions/AutoTester.py
+++ MySolutions/AutoTester.py
@@ -2,35 +2,35 @@ from filecmp import cmp
 from os import system
 
 # количество тестов
-test_count = 16
+TEST_COUNT = 16
 # путь к проекту
-input_path = 'C:\\Users\\Dmitrii\\Desktop\\Дима\\DEV\\Python\\Projects\\pythonProject\\'
+INPUT_PATH = 'C:\\Users\\Dmitrii\\Desktop\\Дима\\DEV\\Python\\Projects\\pythonProject\\'
 # количество успешно пройденных тестов
-success = 0
+SUCCESS = 0
 # запускаемая программа
-test_program = input_path + 's_141.py'
+TEST_PROG = INPUT_PATH + 's_141.py'
 # отчет
-report = input_path + 'report.txt'
+REPORT = INPUT_PATH + 'report.txt'
 
-report_f = open(report, 'w', encoding='utf8')
-for i in range(test_count):
-    file_in_eta = input_path + 'test\\input_' + str(i) + '.txt'
-    file_in = input_path + 'input.txt'
+report_f = open(REPORT, 'w', encoding='utf8')
+for i in range(TEST_COUNT):
+    file_in_eta = INPUT_PATH + 'test\\input_' + str(i) + '.txt'
+    file_in = INPUT_PATH + 'input.txt'
     with open(file_in_eta, 'r', encoding='utf8') as inf:
         with open(file_in, 'w', encoding='utf8') as outf:
             for line in inf:
                 outf.write(line)
-    system('python ' + test_program)
-    file_out_eta = input_path + 'test\\output_' + str(i) + '.txt'
-    file_out = input_path + 'output.txt'
+    system('python ' + TEST_PROG)
+    file_out_eta = INPUT_PATH + 'test\\output_' + str(i) + '.txt'
+    file_out = INPUT_PATH + 'output.txt'
     test_result = cmp(file_out, file_out_eta, shallow=True)
     if test_result:
         report_f.write(f"Тест {i} пройден успешно\n")
-        success += 1
+        SUCCESS += 1
     else:
         report_f.write(f"Тест {i} провален\n")
-if success == test_count:
+if SUCCESS == TEST_COUNT:
     report_f.write("Все тесты пройдены успешно")
 else:
-    report_f.write(f"{test_count - success} тестов пройдено с ошибкой")
+    report_f.write(f"{TEST_COUNT - SUCCESS} тестов пройдено с ошибкой")
 report_f.close()
