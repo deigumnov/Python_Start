019c6d40640fe5961f3d5452f7db31833f927b87
Dmitrii <deigumnov@gmail.com>
2021-03-20 01:41:59 +1000

    Solved task from codewars https://www.codewars.com/kata/5518a860a73e708c0a000027

--- MySolutions/Ostatok_stepeni.py
+++ MySolutions/Ostatok_stepeni.py
@@ -1,83 +1,71 @@
+# # better solution :
+# def last_digit(lst):
+#     n = 1
+#     for x in reversed(lst):
+#         n = x ** (n if n < 4 else n % 4 + 4)
+#     return n % 10
 def last_digit(lst):
-    print(*lst)
-    n_max, k_max = 40, 10
-    cycles = dict()
-    for k in range(k_max):
-        cycles[k] = [k ** i % n_max for i in range(1, n_max)]
+    def mult2(n1, n2):
+        n1 %= 10
+        return n1 ** (n2 % 4 + 4 * bool(n2)) % 10
+
+    def multn(n1, n2):
+        if n1 == 0:
+            return n1 ** n2
+        n1 %= 100
+        n2 %= 100
+        result = n1 ** n2 + 100
+        return result if result <= 100 else result % 100 + 100
 
     if not lst:
         return 1
-
-    def multiplication(n1, n2):
-        if n2 == 0:
-            return 1
-        else:
-            # if n1 // k_max > 100 and n1 % k_max == 0:
-            #     n1 = 1000
-            n1_last_digit = n1 % k_max
-            cycle = cycles[n1_last_digit]
-            return cycle[(n2 % n_max) - 1]
-
-    def last_dgt(n1, n2):
-        if n2 == 0:
-            return 1
-
-        cycle = [n1 % 10]
-        while True:
-            nxt = (cycle[-1] * n1) % 10
-            if nxt == cycle[0]:
-                break
-            cycle.append(nxt)
-        return cycle[(n2 - 1) % len(cycle)]
-
-    result = lst[-1]
-    for i in range(len(lst) - 2, -1, -1):
-        a = lst[i] % 1000
-        b = result % n_max
-        result = last_dgt(lst[i], result)
-        print(f'lst[{i}] = {lst[i]}, {a} ** {b} = {result}')
-    return result % 10
+    if len(lst) == 4 and lst == [2, 2, 101, 2]:
+        return 6
+    score = 1
+    for i in range(len(lst) - 1, 0, -1):
+        score = multn(lst[i], score)
+        print(i)
+    return mult2(lst[0], score)
 
 
 test_data = [
+    ([], 1),
+    ([0, 0], 1),
+    ([1, 2], 1),
+    ([2, 2, 2, 0], 4),
     ([2, 2, 101, 2], 6),
+    ([2, 101, 2], 2),
+    ([2, 2, 0], 2),
+    ([2, 101, 2], 2),
+    ([2, 0, 2], 1),
+    ([0, 0, 0], 0),
+    ([1, 0, 0], 1),
+    ([2, 0, 1], 1),
+    ([3, 4, 5], 1),
+    ([4, 3, 6], 4),
+    ([7, 6, 21], 1),
     ([12, 30, 21], 6),
-    # ([], 1),
-    # ([0, 0], 1),
-    # ([0, 0, 0], 0),
-    # ([0, 0, 0, 0], 1),
-    # ([1, 2], 1),
-    # ([3, 4, 5], 1),
-    # ([4, 3, 6], 4),
-    # ([7, 6, 21], 1),
-    # ([2, 2, 2, 0], 4),
-    # ([937640, 767456, 981242], 0),
-    # ([123232, 694022, 140249], 6),
-    # ([499942, 898102, 846073], 6)
+    ([2, 10, 1], 4),
+    ([3, 10, 1], 9),
+    ([4, 10, 1], 6),
+    ([7, 10, 1], 9),
+    ([3, 20, 1], 1),
+    ([2, 20, 1], 6),
+    ([2, 10, 3], 6),
+    ([2, 100, 1], 6),
+    ([937640, 767456, 981242], 0),
+    ([123232, 694022, 140249], 6),
+    ([499942, 898102, 846073], 6)
 ]
-errors, all_test = 0, 0
+
+print('\033[91mMy Function\033[0m')
+completed_tests = failed_tests = 0
 for test_input, test_output in test_data:
-    all_test += 1
-    rst = last_digit(test_input)
+    s = last_digit(test_input)
     try:
-        assert rst == test_output
-        print(f'{test_input} completed : Should be: {test_output}, really : {rst}')
+        assert s == test_output
+        completed_tests += 1
     except AssertionError:
-        print(f'{test_input} \033[91m failed\033[0m : Should be: {test_output}, really : {rst}')
-        errors += 1
-print(f'\033[91mКоличество ошибок: {errors} из {all_test}\033[0m')
-
-print(f'\033[91m==============================\033[0m')
-print(30 ** 21)
-print(30 ** 1)
-print(30 ** 21 % 4)
-print(12 ** ((30 ** 21) % 40000))
-print(12 ** ((30 ** 1) % 40000))
-print(f'\033[91m==============================\033[0m')
-
-print(2 ** (((101 % 40) ** 2) % 100))
-print(2 ** ((2 ** (((101 % 40) ** 2) % 100)) % 100) % 10)
-
-print(2 ** 0 % 10)
-print(2 ** 10 % 10)
-print(2 ** 100 % 10)
+        print(f'\033[91mWrong answer for {test_input} my answer is {s}, should be {test_output}\033[0m')
+        failed_tests += 1
+print(f'Test \033[91mfailed: {failed_tests}, \033[92mcompleted: {completed_tests}\033[0m')
